5d35536a8296653bf217b064587c6c91b4a831bf
xc145214 <xc145214@gmail.com>
2016-07-07 12:09:36 +0800

    添加实体 和 BaseController

---
+++ src/main/java/com/appStore/controller/BaseController.java
@@ -0,0 +1,30 @@
+/**
+ * **********************************************************************
+ * HONGLING CAPITAL CONFIDENTIAL AND PROPRIETARY
+ * <p/>
+ * COPYRIGHT (C) HONGLING CAPITAL CORPORATION 2012
+ * ALL RIGHTS RESERVED BY HONGLING CAPITAL CORPORATION. THIS PROGRAM
+ * MUST BE USED  SOLELY FOR THE PURPOSE FOR WHICH IT WAS FURNISHED BY
+ * HONGLING CAPITAL CORPORATION. NO PART OF THIS PROGRAM MAY BE REPRODUCED
+ * OR DISCLOSED TO OTHERS,IN ANY FORM, WITHOUT THE PRIOR WRITTEN
+ * PERMISSION OF HONGLING CAPITAL CORPORATION. USE OF COPYRIGHT NOTICE
+ * DOES NOT EVIDENCE PUBLICATION OF THE PROGRAM.
+ * HONGLING CAPITAL CONFIDENTIAL AND PROPRIETARY
+ * ***********************************************************************
+ */
+package com.appStore.controller;
+
+/**
+ *  控制器中基础公共方法的定义，被控制器们继承
+ *
+ *  @author xiachuan at 2016/7/7 11:30。
+ */
+
+public abstract class BaseController {
+
+    public BaseController() {
+    }
+
+
+}
+
---
+++ src/main/java/com/appStore/entity/User.java
@@ -0,0 +1,51 @@
+/**
+ * **********************************************************************
+ * HONGLING CAPITAL CONFIDENTIAL AND PROPRIETARY
+ * <p/>
+ * COPYRIGHT (C) HONGLING CAPITAL CORPORATION 2012
+ * ALL RIGHTS RESERVED BY HONGLING CAPITAL CORPORATION. THIS PROGRAM
+ * MUST BE USED  SOLELY FOR THE PURPOSE FOR WHICH IT WAS FURNISHED BY
+ * HONGLING CAPITAL CORPORATION. NO PART OF THIS PROGRAM MAY BE REPRODUCED
+ * OR DISCLOSED TO OTHERS,IN ANY FORM, WITHOUT THE PRIOR WRITTEN
+ * PERMISSION OF HONGLING CAPITAL CORPORATION. USE OF COPYRIGHT NOTICE
+ * DOES NOT EVIDENCE PUBLICATION OF THE PROGRAM.
+ * HONGLING CAPITAL CONFIDENTIAL AND PROPRIETARY
+ * ***********************************************************************
+ */
+package com.appStore.entity;
+
+import java.io.Serializable;
+
+/**
+ * 用户实体(tableName="user")。
+ *
+ * @author xiachuan at 2016/7/7 12:03。
+ */
+
+public class User implements Serializable{
+
+    /**
+     * column：user_id。
+     * comment：用户ID。
+     */
+    private String userId;
+
+    /**
+     * column：user_name。
+     * comment：用户名。
+     */
+    private String userName;
+
+    /**
+     * column：password。
+     * comment：用户密码。
+     */
+    private String password;
+
+    /**
+     * column：email。
+     * comment：邮箱地址。
+     */
+    private String email;
+}
+
