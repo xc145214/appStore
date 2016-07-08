560636623ab864499ffb8ad878fb4a9c828e327c
xc145214 <xc145214@gmail.com>
2016-07-08 22:24:42 +0800

    update mapper

--- src/main/java/com/appStore/entity/App.java
+++ src/main/java/com/appStore/entity/App.java
@@ -23,7 +23,7 @@ public class App extends BaseEntiy{
      * column:thumbnail_url。
      * comment:文件地址。
      */
-    private String thumbnail_url;
+    private String thumbnailUrl;
 
     /**
      * column:intro。
@@ -49,62 +49,90 @@ public class App extends BaseEntiy{
      */
     private int score =0;
 
-    public App() {
-    }
+    /**
+     * column:top5app。
+     * comment:top5 app。
+     */
+    private String top5app;
 
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
 
     public String getAppid() {
         return appid;
     }
 
+
     public void setAppid(String appid) {
-        this.appid = appid;
+        this.appid = appid == null ? null : appid.trim();
     }
 
-    public String getThumbnail_url() {
-        return thumbnail_url;
+
+    public Integer getScore() {
+        return score;
     }
 
-    public void setThumbnail_url(String thumbnail_url) {
-        this.thumbnail_url = thumbnail_url;
+
+    public void setScore(Integer score) {
+        this.score = score;
     }
 
-    public String getIntro() {
-        return intro;
+
+    public String getTitle() {
+        return title;
     }
 
-    public void setIntro(String intro) {
-        this.intro = intro;
+
+    public void setTitle(String title) {
+        this.title = title == null ? null : title.trim();
     }
+
 
     public String getUrl() {
         return url;
     }
 
+
     public void setUrl(String url) {
-        this.url = url;
+        this.url = url == null ? null : url.trim();
     }
+
+
+    public String getThumbnailUrl() {
+        return thumbnailUrl;
+    }
+
+
+    public void setThumbnailUrl(String thumbnailUrl) {
+        this.thumbnailUrl = thumbnailUrl == null ? null : thumbnailUrl.trim();
+    }
+
+
+    public String getIntro() {
+        return intro;
+    }
+
+
+    public void setIntro(String intro) {
+        this.intro = intro == null ? null : intro.trim();
+    }
+
 
     public String getDeveloper() {
         return developer;
     }
 
+
     public void setDeveloper(String developer) {
-        this.developer = developer;
+        this.developer = developer == null ? null : developer.trim();
     }
 
-    public int getScore() {
-        return score;
+
+    public String getTop5app() {
+        return top5app;
     }
 
-    public void setScore(int score) {
-        this.score = score;
+
+    public void setTop5app(String top5app) {
+        this.top5app = top5app == null ? null : top5app.trim();
     }
+
 }
