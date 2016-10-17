8331b38f2fa92fe0894ff8ea761e092f57192729
xc145214 <xc145214@gmail.com>
2016-10-17 18:13:09 +0800

    create test

---
+++ src/test/java/com/appStore/service/AppServiceTest.java
@@ -0,0 +1,58 @@
+/**
+ * **********************************************************************
+ * HONGLING CAPITAL CONFIDENTIAL AND PROPRIETARY
+ * <p/>
+ * COPYRIGHT (C) HONGLING CAPITAL CORPORATION 2012
+ * ALL RIGHTS RESERVED BY HONGLING CAPITAL CORPORATION. THIS PROGRAM
+ * MUST BE USED  SOLELY FOR THE PURPOSE FOR WHICH IT WAS FURNISHED BY
+ * HONGLING CAPITAL CORPORATION. NO PART OF THIS PROGRAM MAY BE REPRODUCED
+ * OR DISCLOSED TO OTHERS,IN ANY FORM, WITHOUT THE PRIOR WRITTEN
+ * PERMISSION OF HONGLING CAPITAL CORPORATION. USE OF COPYRIGHT NOTICE
+ * DOES NOT EVIDENCE PUBLICATION OF THE PROGRAM.
+ * HONGLING CAPITAL CONFIDENTIAL AND PROPRIETARY
+ * ***********************************************************************
+ */
+package com.appStore.service;
+
+import com.appStore.dao.AppDAO;
+import com.appStore.service.impl.AppServiceImpl;
+import org.easymock.EasyMock;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.test.util.ReflectionTestUtils;
+
+/**
+ *  mock单元测试
+ *
+ *  @author xiachuan at 2016/10/17 18:04。
+ */
+
+public class AppServiceTest {
+
+    public AppServiceTest() {
+    }
+
+    private AppService appService= new AppServiceImpl();
+
+    private AppDAO appDAO = EasyMock.createNiceMock(AppDAO.class);
+
+    /**
+     * 初始化。
+     *
+     * @throws Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+
+        ReflectionTestUtils.setField(appService,"appDAO",AppDAO.class);
+
+
+    }
+
+    @Test
+    public void testName() throws Exception {
+
+
+    }
+}
+
